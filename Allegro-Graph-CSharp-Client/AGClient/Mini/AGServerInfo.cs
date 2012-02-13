399b3555e00c7e2e1407b243183ade57a0b35275
Chenyang WU <shivaw@gmail.com>
2012-02-13 15:18:08 +0800

    - Added RequestUtil

---
+++ Allegro-Graph-CSharp-Client/AGClient/Mini/AGServerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Allegro_Graph_CSharp_Client.AGClient.Mini
+{
+    public class AGServerInfo
+    {
+        public AGServerInfo(string Url, int Port, string Username, string Password)
+        {
+            this.Url = Url;
+            this.Port = Port;
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public string Url { get; private set; }
+        public int Port { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+    }
+}
---
+++ Allegro-Graph-CSharp-Client/AGClient/Util/AGRequestException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Allegro_Graph_CSharp_Client.AGClient.Util
+{
+    public class AGRequestException : Exception
+    {
+        public AGRequestException() : base() { }
+
+        public AGRequestException(string Message) : base(Message) { }
+
+        public AGRequestException(string Message, Exception InnerException) : base(Message, InnerException) { }
+    }
+}
