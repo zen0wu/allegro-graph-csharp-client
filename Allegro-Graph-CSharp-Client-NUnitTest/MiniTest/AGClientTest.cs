926a582c7067e7e8bab059eba182ab910424fb79
happyblizzard <fxfcn@126.com>
2012-02-15 23:43:57 +0800

    2012-2-15

---
+++ Allegro-Graph-CSharp-Client-NUnitTest/MiniTest/AGClientTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using NUnit.Framework;
+using Allegro_Graph_CSharp_Client.AGClient.Util;
+using Allegro_Graph_CSharp_Client.AGClient.Mini;
+
+namespace Allegro_Graph_CSharp_Client_NUnitTest.MiniTest
+{
+    /// <summary>
+    /// 测试AGClient类
+    /// </summary>
+    [TestFixture]
+    class AGClientTest
+    {
+        private AGClient agClient;
+        private AGServerInfo server;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            server = new AGServerInfo("http://172.16.2.21:10035", "chainyi", "chainyi123");
+            agClient = new AGClient(server);
+        }
+
+        ///<summary>
+        /// 测试构造函数以及同样的参数是否返回同样的对象
+        ///<summary>
+        [Test]
+        public void SameObjectTest()
+        {
+            AGClient agClient1 = new AGClient(server);
+            AGClient agClient2 = new AGClient(server);
+            Assert.AreNotSame(agClient1, agClient2);
+        }
+
+        /// <summary>
+        /// 测试 GetVersion()
+        /// </summary>
+        [Test]
+        public void GetVersionTest()
+        {
+            string version = agClient.GetVersion();
+            bool result = string.IsNullOrEmpty(version);
+            Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// 测试 ListCatalogs()
+        /// </summary>
+        [Test]
+        public void ListCatalogsTest()
+        {
+            string[] catalogs = agClient.ListCatalogs();
+            Assert.IsNotEmpty(catalogs);
+        }
+
+        /// <summary>
+        /// 测试 OpenCatalog()
+        /// </summary>
+        [Test]
+        public void OpenCatalogTest()
+        {
+            string catalogName = "zhishime";
+            //Assert.IsInstanceOf(Type.GetType("Allegro_Graph_CSharp_Client.AGClient.Mini.AGCatalog"), agClient.OpenCatalog(catalogName));
+            bool result = agClient.OpenCatalog(catalogName) is AGCatalog;
+            Assert.IsTrue(result);
+        }
+    }
+}
