e79fe8f0e4b195c6a6b5c3582e250a645b5f58b8
Chenyang WU <shivaw@gmail.com>
2012-02-19 00:30:53 +0800

    - Added GetStatements, EvalSPARQLQuery in AGRepository, to be tested

--- Allegro-Graph-CSharp-Client/AGClient/Mini/AGRepository.cs
+++ Allegro-Graph-CSharp-Client/AGClient/Mini/AGRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 
 using Newtonsoft.Json;
 using Allegro_Graph_CSharp_Client.AGClient.Util;
@@ -93,5 +94,78 @@ namespace Allegro_Graph_CSharp_Client.AGClient.Mini
         {
             AGRequestService.DoReq(this, "POST", "/statememts/delete?ids=true", JsonConvert.SerializeObject(Ids));
         }
+
+        /// <summary>
+        /// 执行Sparql查询
+        /// </summary>
+        /// <param name="Query">查询</param>
+        /// <param name="Infer">推理类型</param>
+        /// <param name="Context">上下文</param>
+        /// <param name="NamedContext">命名的上下文</param>
+        /// <param name="Bindings">已知的变量绑定</param>
+        /// <param name="CheckVariables">是否检查不存在的变量</param>
+        /// <param name="Limit">返回结果的最多个数</param>
+        /// <param name="Offset">跳过部分返回结果</param>
+        /// <returns></returns>
+        public string EvalSPARQLQuery(string Query, string Infer = "false", string Context = null, string NamedContext = null,
+            Dictionary<string, string> Bindings = null, bool CheckVariables = false, int Limit = -1, int Offset = -1)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("query", Query);
+            parameters.Add("queryLn", "sparql");
+            parameters.Add("infer", Infer);
+            if (Context != null) parameters.Add("context", Context);
+            if (NamedContext != null) parameters.Add("namedContext", NamedContext);
+            if (Bindings != null)
+            {
+                foreach (string vari in Bindings.Keys) 
+                    parameters.Add("$" + vari, HttpUtility.UrlEncode(Bindings[vari]));
+            }
+            parameters.Add("checkVariables", CheckVariables.ToString());
+            if (Limit >= 0) parameters.Add("limit", Limit.ToString());
+            if (Offset >= 0) parameters.Add("offset", Offset.ToString());
+            return AGRequestService.DoReqAndGet(this, "GET", "", parameters);
+        }
+
+        /// <summary>
+        /// 返回给定条件的triples
+        /// </summary>
+        /// <param name="Subj">主语限制</param>
+        /// <param name="Pred">谓语限制</param>
+        /// <param name="Obj">宾语限制</param>
+        /// <param name="Context">上下文限制</param>
+        /// <param name="Infer">推理类型，默认为false，即不推理</param>
+        /// <param name="Limit">返回结果的最多个数</param>
+        /// <param name="Offset">跳过部分返回结果</param>
+        /// <returns></returns>
+        public string GetStatements(string[] Subj, string[] Pred, string[] Obj, string[] Context, string Infer = "false", 
+            int Limit = -1, int Offset = -1)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            Action<string, string[]> addArrayParam = delegate(string Key, string[] param)
+            {
+                if (param.Length > 0)
+                {
+                    if (param.Length == 1)
+                        parameters.Add(Key, param[0]);
+                    else
+                        parameters.Add(Key, JsonConvert.SerializeObject(param));
+                }
+            };
+            addArrayParam("subj", Subj);
+            addArrayParam("pred", Pred);
+            addArrayParam("obj", Obj);
+            addArrayParam("context", Context);
+            if (Limit >= 0) parameters.Add("limit", Limit.ToString());
+            if (Offset >= 0) parameters.Add("offset", Offset.ToString());
+            return AGRequestService.DoReqAndGet(this, "GET", "/statements", parameters);
+        }
+
+        /*
+         * 目前的问题
+         * 上面两个函数的返回类型
+         * subjEnd的作用
+         * subj参数能不能用数组传递
+        */
     }
 }
