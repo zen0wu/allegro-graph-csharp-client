1220a4b021d866f392a4a93298060541a07f5a99
happyblizzard <fxfcn@126.com>
2012-03-21 19:58:06 +0800

    2012-3-20

--- Allegro-Graph-CSharp-Client/AGClient/OpenRDF/Model/GeoType.cs
+++ Allegro-Graph-CSharp-Client/AGClient/OpenRDF/Model/GeoType.cs
@@ -8,20 +8,36 @@ namespace Allegro_Graph_CSharp_Client.AGClient.OpenRDF.Model
 {
     public class GeoType
     {
-        string Cartesian = "CARTESIAN";
-        string Spherical = "SPHERICAL";
+        public static readonly string Cartesian = "CARTESIAN";
+        public static readonly string Spherical = "SPHERICAL";
         string system;
-        RepositoryConnection connection = null;
+        //RepositoryConnection connection = null;
         string scale;
-        string unit;
-        int xMin;
-        int xMax;
-        int yMin;
-        int yMax;
-        int latMin;
-        int latMax;
-        int longMin;
-        int longMax;
-        string miniGeoType = null;
+        float unit;
+        float xMin;
+        float xMax;
+        float yMin;
+        float yMax;
+        float latMin;
+        float latMax;
+        float longMin;
+        float longMax;
+        //string miniGeoType = null;
+        public GeoType(string sys, string scale, float unit,
+                       float xMin = 0, float xMax = 0, float yMin = 0, float yMax = 0,
+                       float latMin = 0, float latMax = 0, float longMin = 0, float longMax = 0)
+        {
+            this.system = sys;
+            this.scale = scale;
+            this.unit = unit;
+            this.xMin = xMin;
+            this.xMax = xMax;
+            this.yMin = yMin;
+            this.yMax = yMax;
+            this.latMin = latMin;
+            this.latMax = latMax;
+            this.longMin = longMin;
+            this.longMax = longMax;
+        }
     }
 }
---
+++ Allegro-Graph-CSharp-Client/AGClient/OpenRDF/RepositoryUtil/RepositoryConnectionForGeospatialReasoning.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Allegro_Graph_CSharp_Client.AGClient.OpenRDF.RepositoryUtil
+{
+    public partial class RepositoryConnection
+    {
+
+        public string[] _getStatementsInRegion(string subject,string predicate,object  region,string[] contexts,int limit=-1,int offset=-1)
+        {
+        }
+    }
+}
